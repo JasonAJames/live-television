2eb3fba39cb3e8a920254201da4db183379c9d18
Jason James <jasonajames@live.com>
2018-03-16 10:19:22 -0700

    fixing Cannot GET error by modifying app.use function with heroku headers x-forwarded-proto  server.js

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 // //Install express server
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
+
+// app.configure('production', => {
+//     app.use((req, res, next) => {
+//       if (req.header 'x-forwarded-proto' !== 'https')
+//         res.redirect(`https://${req.header('host')}${req.url}`)
+//       else
+//         next()
+//     })
+//   })
+
+  app.use((req, res, next) => {
+    if (req.header ['x-forwarded-proto'] !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
 
 // // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/dist'));
@@ -8,34 +24,34 @@
 // // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
 
-// server.js
-const express = require('express');
-const app = express();
+// // server.js
+// const express = require('express');
+// const app = express();
 
-const path = require('path');
+// const path = require('path');
 
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+// // For all GET requests, send back index.html
+// // so that PathLocationStrategy can be used
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+//   });
 
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
-//
+// // If an incoming request uses
+// // a protocol other than HTTPS,
+// // redirect that request to the
+// // same url but with HTTPS
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
+// // Instruct the app
+// // to use the forceSSL
+// // middleware
+// app.use(forceSSL());
+// //

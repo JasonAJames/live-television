3466a2901c284e6a3c88824a222698cd77dbd1a5
Jason James <jasonajames@live.com>
2018-03-17 10:59:25 -0700

    Updated server.js app.use /* dist

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
 app.use('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    app.use( express.static(__dirname + '/dist' ) );
     });
 
 // app.use( express.static(__dirname + '/dist' ) );

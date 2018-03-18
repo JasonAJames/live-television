9eb3aa2ee6d7fb34b7eb0d8ce37b4b5c282505c3
Jason James <jasonajames@live.com>
2018-03-18 15:13:19 -0700

    updated package.js start to node server.js to src/app

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
-app.use( express.static(__dirname + './src/app' ) );
+app.use( express.static(__dirname + 'src/app' ) );
 
 var listener = server.listen(process.env.PORT || 8080, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 5000

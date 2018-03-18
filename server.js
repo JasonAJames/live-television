48186a5b1432f5161d98812eee282eb7926a5a71
Jason James <jasonajames@live.com>
2018-03-18 15:32:16 -0700

    reverted package.js start node server.js to /dist

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
-app.use( express.static(__dirname + './dist' ) );
+// app.use('/', function(req, res) {
+//     express.static(__dirname + '/dist' );
+//     });
+
+app.use( express.static(__dirname + '/dist' ) );
 
 var listener = server.listen(process.env.PORT || 8080, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 5000

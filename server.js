943d44e7e7bbf0dce80524ebf04ae0771f96f290
Jason James <jasonajames@live.com>
2018-03-20 13:41:51 -0700

    added /* to server.js server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}) );
 
 app.use( express.static(__dirname + '/dist' ) );
 
-app.get('*', (req, res) =>{
+app.get('/*', (req, res) =>{
     res.sendFile(path.resolve(__dirname, '/dist'));
   });
 

627a7f34e460d1970bba992397aabcaea2494d40
Jason James <jasonajames@live.com>
2018-03-27 15:21:42 -0700

    updated index.html baseurl to  /src/

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Routes, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
 
-stationName = 'SoCal Television'
+stationName = 'SoCal Television';
+
 
 }

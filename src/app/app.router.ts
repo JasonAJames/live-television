401e9a7177b5a65143ffa8f05cd8456532c5f8eb
Jason James <jasonajames@live.com>
2018-04-03 17:43:12 -0700

    updated app.router with live feed

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { LiveComponent } from './live/live.component';
 import { HomeComponent } from './stations-main/socaltelevision/home.component';
 import { StationsMainComponent } from './stations-main/stations-main.component';
 import { Ktla5Component } from './stations-main/ktla5/ktla5.component';
@@ -11,6 +12,7 @@ import { Ktla5Component } from './stations-main/ktla5/ktla5.component';
 
 export const router: Routes = [
     { path: 'home', component: HomeComponent},
+    { path: 'live', component: LiveComponent},
     { path: 'about', component: AboutComponent},
     { path: 'stations-main', component: StationsMainComponent},
     { path: 'ktla5', component: Ktla5Component},

e216f6dd25ebfde166f76c6a6d1aed73d7ad33ab
Jason James <jasonajames@live.com>
2018-04-05 10:51:46 -0700

    updated facebook live feed page

--- src/app/live/live.component.ts
+++ src/app/live/live.component.ts
@@ -14,5 +14,6 @@ export class LiveComponent implements OnInit {
 
   stationName = 'SoCal Television LIVE';
   videoID = "2F2049949125045139";
+  videoSrc = 'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FSoCalTelevision%2Fvideos%2F2052326111474107%2F&width=400';
 
 }

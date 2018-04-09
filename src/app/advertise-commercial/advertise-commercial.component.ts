293f1ae1debffe87bb94b4ed98b4d61ffbaf7b30
Jason James <jasonajames@live.com>
2018-04-09 11:42:17 -0700

    clearing component advertising component ts

--- src/app/advertise-commercial/advertise-commercial.component.ts
+++ src/app/advertise-commercial/advertise-commercial.component.ts
@@ -14,47 +14,6 @@ export class AdvertiseCommercialComponent implements OnInit {
 
   ngOnInit() {
   }
-addScript: boolean = false;
-finalAmount: number = 1;
-paypalConfig = {
-  env: 'sandbox',
-  client: {
-    sandbox: "AYNt1FW36K2XuUe4uv9nZOnqnp5X8K6Ms6qtd0igsRK9CxBXpnBPQ9VuPEuSZzs-aV3zosxUMqo5zFI2"
-  },
-  commit: true,
-  payment: (data, actions) => {
-    return actions.payment.create({
-      payment: {
-        transactions: [
-          { amount: { total: this.finalAmount, currency: 'USD'}}
-        ]
-      }
-    });
-  },
-  onAuthorize: (data, actions) => {
-    return actions.payment.execute().then((payment) => {
-      // TODO Payment Successful
 
-    })
-  }
-};
-
-ngAfterViewChecked(): void {
-  if(!this.addScript) {
-    this.addPaypalScript().then(() => {
-        paypal.Button.render(this.paypalConfig, '#paypalSubmit_BTN')
-    })
-  }
-}
-
-addPaypalScript() {
-  this.addScript = true;
-  return new Promise((resolve, reject) => {
-    let scripttagElement = document.createElement('script');
-    scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
-    scripttagElement.onload = resolve;
-    document.body.appendChild(scripttagElement);
-  })
-}
 
 }

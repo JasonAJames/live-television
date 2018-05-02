3aa264a8b188b64f9d2d896272d1fa262b4166c3
Jason James <jasonajames@live.com>
2018-05-02 13:28:44 -0700

    removed videoUrl property binding

--- src/app/live/live.component.ts
+++ src/app/live/live.component.ts
@@ -14,5 +14,5 @@ export class LiveComponent implements OnInit {
 
   stationName = 'SoCal Television LIVE';
   description = "Man reportedly barricaded in La Crescenta home. Suspect was previously sentenced to five years probation for stalking actress Sandra Bullock, according to TMZ."
-  videoUrl = "https://www.facebook.com/fox11la/videos/10160446573060553/"
+
 }

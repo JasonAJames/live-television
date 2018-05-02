bb2db90efe634448361ab77e01e053b2bbfdf89d
Jason James <jasonajames@live.com>
2018-05-01 21:45:14 -0700

    added google analytics script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router, NavigationEnd} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -24,4 +25,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SoCalTelevision.com';
+
+
 }

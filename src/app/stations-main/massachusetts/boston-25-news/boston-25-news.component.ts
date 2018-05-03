9ecdc1598ba1e68ff9684158ddb0d412c9294494
Jason James <jasonajames@live.com>
2018-05-03 11:45:28 -0700

    moving width and hieght styling to css file for boston-25-news

--- src/app/stations-main/massachusetts/boston-25-news/boston-25-news.component.ts
+++ src/app/stations-main/massachusetts/boston-25-news/boston-25-news.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-boston-25-news',
   templateUrl: './boston-25-news.component.html',
-  styleUrls: ['./boston-25-news.component.css']
+  styleUrls: ['./boston-25-news.component.css', '../../../app.component.css']
 })
 export class Boston25NewsComponent implements OnInit {
 

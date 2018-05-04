aa396b6f5f724c0bb6a7726f5f539691c82e03a5
Jason James <jasonajames@live.com>
2018-05-04 09:08:23 -0700

    reverting to fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { ArizonaComponent } from './stations-main/arizona/arizona.component';
 import { ColoradoComponent } from './stations-main/colorado/colorado.component';
 import { Colorado9newsComponent } from './stations-main/colorado/colorado-9news/colorado-9news.component';
 import { Nbc11newsComponent } from './stations-main/colorado/nbc11news/nbc11news.component';
-import { ContactComponent } from './contact/contact.component';
+
 
 
 @NgModule({
@@ -63,7 +63,7 @@ import { ContactComponent } from './contact/contact.component';
     ColoradoComponent,
     Colorado9newsComponent,
     Nbc11newsComponent,
-    ContactComponent
+
   ],
   imports: [
     BrowserModule,
@@ -76,7 +76,7 @@ import { ContactComponent } from './contact/contact.component';
       { path: 'local-reviews', component: LocalReviewsComponent},
       { path: 'advertise-commercial', component: AdvertiseCommercialComponent},
       { path: 'about', component: AboutComponent},
-      { path: 'contact', component: ContactComponent },
+
       { path: 'stations-main', component: StationsMainComponent},
 
       { path: 'arizona-tv-stations', component: ArizonaComponent},

81298fcae4a4ffa6889e4d5857a88868b981fd79
Jason James <jasonajames@live.com>
2018-05-16 11:52:04 -0700

    added live news conference feed

--- src/app/stations-main/live/live.component.ts
+++ src/app/stations-main/live/live.component.ts
@@ -13,8 +13,10 @@ export class LiveComponent implements OnInit {
   }
 
   offlineMessage1 = "WATCH LIVE: A deadly explosion has been reported in Aliso Viejo."
+  subdesc = "To Watch the LIVE News Conference UPDATE RE: "
 
 
   stationName = 'SoCal Television LIVE'
   description = this.offlineMessage1
+  subdescription = this.subdesc
 }

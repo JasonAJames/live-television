52dbf1863114cc56cca6d7f119956247761e2584
Jason James <jasonajames@live.com>
2018-07-03 23:28:32 -0700

    added independence-day LIVE styling

--- src/app/independanceday/independanceday.component.ts
+++ src/app/independanceday/independanceday.component.ts
@@ -15,55 +15,57 @@ export class IndependancedayComponent implements OnInit {
   offlineMessage1 = "SoCal Television LIVE COVERAGE"
   subdesc = "The City of Corona is hosting its 20th Anniversary Main Street USA Parade, presented by the Corona Rotary, on Wednesday, July 4 beginning at 9 a.m. at the intersection of Main and Ontario and moves north to Olive Street."
   paradeinfo = `
-  FOURTH OF JULY IN CORONA PARADE INFORMATION:<br/>
-The City of Corona is hosting its 20th Anniversary Main Street USA Parade, presented by the Corona Rotary, on Wednesday, July 4 beginning at 9 a.m. at the intersection of Main and Ontario and moves north to Olive Street. Parade entries will line-up on Ontario Avenue, Main Street and Pueblo Road. 
-<br/><br/>
-ROAD CLOSURES
-<br/>
-The following streets will be posted “No Parking” and closed to through traffic from approximately 6:30 a.m. to 12 p.m. on Wednesday, July 4, 2018:
-<br/>
-Main Street between Ontario Avenue and Grand Boulevard<br/>
-Chatam Circle from the cul-de-sac to Summerset<br/>
-Pueblo Road between Main Street and Belle Avenue<br/>
-Olive Street between Main Street and Belle Avenue<br/>
-Olive Street between Main Street and South Victoria Avenue<br/>
-Please anticipate increased traffic congestion in the area on July 4.<br/>
-<br/>
-SANTANA PARK FOOD FESTIVAL &amp; FIREWORKS<br/>
-The fun will continue at Santana Park beginning at 5 p.m. with live music and food vendors.<br/>
-<br/>
-ENTERTAINMENT<br/>
-The entertainment begins at 5 p.m. with Kids Rock Free School of Music. The young group has performed at numerous events around Southern California and received rave reviews for their shows.<br/>
+  FOURTH OF JULY IN CORONA
 
-Returning to the Independence Day stage is Corona’s own – Yard Sale! Yard Sale is an 8-piece, high-energy, horn-driven, powerhouse band that covers all the best dance hits from the 70’s through today. Yard Sale has been a regular on the Downtown Disney stage since 2013 and has played over 70 summer concert series events and countless private/corporate functions throughout Southern California. The camaraderie and energy on stage is contagious, and they’ll keep the dance floor full!<br/>
-<br/>
-FIREWORKS<br/>
-The celebration concludes with the eagerly anticipated firework show to occur at sundown.<br/>
-<br/>
-PARKING<br/>
-
-Santana Parking Lot- $5 parking fee for up-close parking spots<br/>
-Free parking at Crossroads Christian Church and School and Lee Pollard High School<br/><br/>
-NOTICE<br/><br/>
-
-No Dogs or pets allowed at the park<br/>
-No gas/propane BBQs (Charcoal only)<br/>
-No alcohol or cigarettes (including vaping) allowed in the park<br/>
-No drones<br/><br/>
-ROAD CLOSURES<br/>
-<br/>
-Ontario Ave from Magnolia to Fullerton<br/>
-Magnolia from Main Street to Ontario<br/>
-Kellogg from Kingfisher to Pimpernel<br/>
-Fullerton from Old Temescal Canyon to McMackin<br/>
-Santana from Magnolia to Hidden Creek<br/>
-Garretson from CL Fleming to Santana<br/>
-Twinleaf from Lacey Oak to Santana<br/>
-Please anticipate increased traffic congestion in the area on July 4. <br/><br/>
-
-Closures are scheduled to begin at 11 a.m. and all closures are expected to be in place by 5 p.m. but all closures are subject to closure at any time.<br/>
-<br/>
-No one will be allowed through any street closure for safety reasons.<br/>
+  PARADE:
+  The City of Corona is hosting its 20th Anniversary Main Street USA Parade, presented by the Corona Rotary, on Wednesday, July 4 beginning at 9 a.m. at the intersection of Main and Ontario and moves north to Olive Street. Parade entries will line-up on Ontario Avenue, Main Street and Pueblo Road. 
+  
+  ROAD CLOSURES
+  
+  The following streets will be posted “No Parking” and closed to through traffic from approximately 6:30 a.m. to 12 p.m. on Wednesday, July 4, 2018:
+  
+  Main Street between Ontario Avenue and Grand Boulevard
+  Chatam Circle from the cul-de-sac to Summerset
+  Pueblo Road between Main Street and Belle Avenue
+  Olive Street between Main Street and Belle Avenue
+  Olive Street between Main Street and South Victoria Avenue
+  Please anticipate increased traffic congestion in the area on July 4.
+  
+  SANTANA PARK FOOD FESTIVAL & FIREWORKS
+  The fun will continue at Santana Park beginning at 5 p.m. with live music and food vendors.
+  
+  ENTERTAINMENT
+  The entertainment begins at 5 p.m. with Kids Rock Free School of Music. The young group has performed at numerous events around Southern California and received rave reviews for their shows.
+  
+  Returning to the Independence Day stage is Corona’s own – Yard Sale! Yard Sale is an 8-piece, high-energy, horn-driven, powerhouse band that covers all the best dance hits from the 70’s through today. Yard Sale has been a regular on the Downtown Disney stage since 2013 and has played over 70 summer concert series events and countless private/corporate functions throughout Southern California. The camaraderie and energy on stage is contagious, and they’ll keep the dance floor full!
+  
+  FIREWORKS
+  The celebration concludes with the eagerly anticipated firework show to occur at sundown.
+  
+  PARKING
+  
+  Santana Parking Lot- $5 parking fee for up-close parking spots
+  Free parking at Crossroads Christian Church and School and Lee Pollard High School
+  NOTICE
+  
+  No Dogs or pets allowed at the park
+  No gas/propane BBQs (Charcoal only)
+  No alcohol or cigarettes (including vaping) allowed in the park
+  No drones
+  ROAD CLOSURES
+  
+  Ontario Ave from Magnolia to Fullerton
+  Magnolia from Main Street to Ontario
+  Kellogg from Kingfisher to Pimpernel
+  Fullerton from Old Temescal Canyon to McMackin
+  Santana from Magnolia to Hidden Creek
+  Garretson from CL Fleming to Santana
+  Twinleaf from Lacey Oak to Santana
+  Please anticipate increased traffic congestion in the area on July 4. 
+  
+  Closures are scheduled to begin at 11 a.m. and all closures are expected to be in place by 5 p.m. but all closures are subject to closure at any time.
+  
+  No one will be allowed through any street closure for safety reasons.
   `
 
   stationName = 'SoCal Television LIVE'

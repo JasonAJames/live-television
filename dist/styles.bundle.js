e1f68fd7a1952fe60b9f8890164fad8b86981da7
Jason James <jasonajames@live.com>
2018-07-03 18:31:38 -0700

    added verticle-align top class to HomeBG

--- dist/styles.bundle.js
+++ dist/styles.bundle.js
@@ -3,7 +3,7 @@ webpackJsonp(["styles"],{
 /***/ "./node_modules/raw-loader/index.js!./node_modules/postcss-loader/lib/index.js??embedded!./src/styles.css":
 /***/ (function(module, exports) {
 
-module.exports = "\n@import url('https://bootswatch.com/4/spacelab/bootstrap.min.css');\n/* @import url('https://bootswatch.com/4/cyborg/bootstrap.min.css'); */\n.HomeBG {\n    background-image: url('https://s3-us-west-2.amazonaws.com/socaltelevision/HomeBG.png');\n    background-repeat: no-repeat;\n    background-attachment: fixed;\n    background-position: center;\n}"
+module.exports = "\n@import url('https://bootswatch.com/4/spacelab/bootstrap.min.css');\n/* @import url('https://bootswatch.com/4/cyborg/bootstrap.min.css'); */\n.HomeBG {\n    background-image: url('https://s3-us-west-2.amazonaws.com/socaltelevision/HomeBG.png');\n    background-repeat: no-repeat;\n    background-attachment: fixed;\n    background-position: center;\n    vertical-align: top;\n}"
 
 /***/ }),
 

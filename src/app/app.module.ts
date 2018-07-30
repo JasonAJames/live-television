4f8154b6c04929b4eed14b2a6fc8a36d32917481
Jason James <jasonajames@live.com>
2018-07-30 07:58:41 -0700

    added usopenofsurfing feed 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import { HoroscopesComponent } from './extras/horoscopes/horoscopes.component';
 import { StockMarketComponent } from './extras/stock-market/stock-market.component';
 import { IndependancedayComponent } from './independanceday/independanceday.component';
 import { IcecreamComponent } from './national-day/icecream/icecream.component';
+import { VansusopenofsurfingLiveComponent } from './vansusopenofsurfing-live/vansusopenofsurfing-live.component';
 
 
 
@@ -85,7 +86,8 @@ import { IcecreamComponent } from './national-day/icecream/icecream.component';
     HoroscopesComponent,
     StockMarketComponent,
     IndependancedayComponent,
-    IcecreamComponent
+    IcecreamComponent,
+    VansusopenofsurfingLiveComponent
 
   ],
   imports: [
@@ -111,7 +113,7 @@ import { IcecreamComponent } from './national-day/icecream/icecream.component';
       { path: 'fox10-phoenix', component: Fox10phoenixComponent},
 
       { path: 'national-day/icecream', component: IcecreamComponent},
-
+      { path: 'usopenofsurfing', component: VansusopenofsurfingLiveComponent},
       { path: 'california-tv-stations', component: CaliforniaComponent },
       { path: 'ktla5', component: Ktla5Component},
       { path: 'fox11la', component: Fox11laComponent},

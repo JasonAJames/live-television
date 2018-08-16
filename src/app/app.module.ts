b658374e10481a5f7dbdaea6202587fbec29f781
Jason James <jasonajames@live.com>
2018-08-16 08:16:03 -0700

    added national roller coaster day view as home view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -141,8 +141,8 @@ import { RollerCoasterComponent } from './national-day/roller-coaster/roller-coa
       { path: 'kitv4-hawaii', component: Kitv4hawaiiComponent},
       { path: 'khon2-hawaii', component: Khon2hawaiiComponent},
 
-      { path: '', redirectTo: '/home', pathMatch: 'full'},
-      { path: '**', component: HomeComponent}
+      { path: '', redirectTo: '/national-day/rollercoaster', pathMatch: 'full'},
+      { path: '**', component: RollerCoasterComponent}
     ], {useHash : true}),
     routes
   ],

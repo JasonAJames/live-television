befdb3aa3967a9d55363635be02a9b53a51625f4
Jason James <jasonajames@live.com>
2018-09-27 14:41:54 -0700

    aded script.js to index.html

--- src/app/home-video/home-video.component.ts
+++ src/app/home-video/home-video.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home-video',
   templateUrl: './home-video.component.html',
   styleUrls: ['./home-video.component.css']
 })
+
 export class HomeVideoComponent implements OnInit {
 
   constructor() { }

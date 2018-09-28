fe6b04c762db42a68ecee7663a1f709efa751b27
Jason James <jasonajames@live.com>
2018-09-27 17:36:49 -0700

    testing binding of video tag

--- src/app/home-video/home-video.component.ts
+++ src/app/home-video/home-video.component.ts
@@ -10,6 +10,13 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeVideoComponent implements OnInit {
 
+  currentvideo = `
+  <video controls poster="http://thenewcode.com/assets/images/vid-glacier.jpg" playsinline>
+  <source src="https://s3-us-west-2.amazonaws.com/jppmsolutions/Videos/CajalcoLabyrinth_CoronaCalifornia.mp4.mp4" type="video/mp4">
+  <source src="https://s3-us-west-2.amazonaws.com/jppmsolutions/Videos/CajalcoLabyrinth_CoronaCalifornia.webmhd.webm" type="video/webm">
+</video>
+  `
+
   constructor() { }
 
   ngOnInit() {

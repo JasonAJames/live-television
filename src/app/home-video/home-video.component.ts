be8db573c74d997e7eec45c60b3c5c9a4fdb655a
Jason James <jasonajames@live.com>
2018-09-27 17:24:21 -0700

    added s3 video binding

--- src/app/home-video/home-video.component.ts
+++ src/app/home-video/home-video.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeVideoComponent implements OnInit {
 
-  
+  v1_root = "https://s3-us-west-2.amazonaws.com/jppmsolutions/Videos/CajalcoLabyrinth_CoronaCalifornia";
 
   constructor() { }
 

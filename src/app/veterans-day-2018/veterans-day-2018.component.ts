0cc06533f0c35e0b1bdd0ecb8e35b1cf16832c24
Jason James <jasonajames@live.com>
2018-11-09 16:03:35 -0800

    changed width on event picture

--- src/app/veterans-day-2018/veterans-day-2018.component.ts
+++ src/app/veterans-day-2018/veterans-day-2018.component.ts
@@ -13,5 +13,5 @@ export class VeteransDay2018Component implements OnInit {
   }
   stationName = "SoCalTelevision"
   description = "2018 VETERANS DAY FREE MEALS, DISCOUNTS & CELEBRATIONS"
-  subdescription = "We're having a Birthday Celebration this Saturday, November 10th, at Sons of Liberty Aleworks. This will be an all day outdoor parking lot event celebrating the 243rd Anniversary of the founding of the, then, Continental Marines in 1775, later to become the United States Marine Corps."
+  subdescription = "Son's of Liberty Aleworks is having a Birthday Celebration this Saturday, November 10th. This will be an all day outdoor parking lot event celebrating the 243rd Anniversary of the founding of the, then, Continental Marines in 1775, later to become the United States Marine Corps. Come Join Us in Celebration."
 }

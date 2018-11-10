c2ef5605b34267fe4cebc5011db50b6fc93148c5
Jason James <jasonajames@live.com>
2018-11-09 18:50:59 -0800

    added social share

--- src/app/veterans-day-2018/veterans-day-2018.component.ts
+++ src/app/veterans-day-2018/veterans-day-2018.component.ts
@@ -13,6 +13,8 @@ export class VeteransDay2018Component implements OnInit {
   ngOnInit() {
   }
   stationName = "SoCalTelevision"
+  title = "Son's of Liberty Aleworks 243rd Marine Birthday Celebration"
+  url = "http://www.socaltelevision.com/#/veterans-day-2018"
   description = "2018 VETERANS DAY FREE MEALS, DISCOUNTS & CELEBRATIONS"
   subdescription = "Son's of Liberty Aleworks is having a Birthday Celebration this Saturday, November 10th. This will be an all day outdoor parking lot event celebrating the 243rd Anniversary of the founding of the, then, Continental Marines in 1775, later to become the United States Marine Corps. Come Join Us in Celebration."
 }

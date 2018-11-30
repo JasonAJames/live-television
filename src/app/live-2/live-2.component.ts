6776c83312363d315d4719f4f02817fb7660aa30
Jason James <jasonajames@live.com>
2018-11-30 13:08:59 -0800

    added live vid earthquake ak

--- src/app/live-2/live-2.component.ts
+++ src/app/live-2/live-2.component.ts
@@ -13,7 +13,7 @@ export class Live2Component implements OnInit {
   }
 
   offlineMessage1 = "SoCal Television LIVE BREAKING NEWS"
-  subdesc = `The San Ysidro Port of Entry is closed after the migrant caravan rushed the border to demand asylum.`
+  subdesc = `A 7.0 earthquake struck near Anchorage, Alaska this morning. Though a local TV station was knocked off the air`
 
 
   stationName = 'SoCal Television LIVE'

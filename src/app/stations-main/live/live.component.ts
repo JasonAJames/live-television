adb506467492c05827bbc97d81564680c022e442
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-01-17 09:10:29 -0800

    police pursuit

--- src/app/stations-main/live/live.component.ts
+++ src/app/stations-main/live/live.component.ts
@@ -13,7 +13,7 @@ export class LiveComponent implements OnInit {
   }
 
   offlineMessage1 = "SoCal Television #LIVE"
-  subdesc = "LIVE: Police are pursuing a suspect in the Culver City area."
+  subdesc = "#LIVE: A high speed chase is underway in Dallas, Texas."
 
 
   stationName = 'SoCal Television Breaking News LIVE'

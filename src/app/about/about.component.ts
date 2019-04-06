ca363d8759edb1b22e72c3981ec1d1011741998c
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-04-06 10:59:06 -0700

    testing social-share-component fa icons css linking on about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: '../about/about.component.html',
-  styleUrls: ['../about/about.component.css']
+  styleUrls: ['../about/about.component.css','../components/social-share-icons/social-share-icons.component.css']
 })
 export class AboutComponent implements OnInit {
 
@@ -11,5 +11,17 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  // Social Sharing binding data
+  title = "SoCal Television"
+  header_link = "Special Spots"
+  url = "https://www.socaltelevision.com/+#+/about"
+  description = 'Special Spots';
+  subdescription = 'We venture throughtout Southern California and beyond, to showcase attractions, events, entertainment, places of interest, restaurant reviews, and more...'
+  summary = 'We venture throughtout Southern California and beyond, to showcase attractions, events, entertainment, places of interest, restaurant reviews, and more...'
+  source_url = this.url
+  subject = "Check Out this SoCalTelevision.com website"
+  body = 'SoCalTelevision.com Special Spots ventures throughtout Southern California and beyond, to showcase attractions, events, entertainment, places of interest, restaurant reviews, and more...'
+
 
 }

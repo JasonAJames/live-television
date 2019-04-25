9cd765706b68b763a76e5ae2e3c5cde64bf6e7eb
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-04-24 17:27:18 -0700

    police pursuit live

--- src/app/stations-main/live/live.component.ts
+++ src/app/stations-main/live/live.component.ts
@@ -13,7 +13,7 @@ export class LiveComponent implements OnInit {
   }
 
   offlineMessage1 = "SoCal Television #LIVE"
-  subdesc = "President Donald Trump is visiting the border right now to tour a recently built portion of fence."
+  subdesc = "Watch live: Authorities are in pursuit of a driver in the South L.A."
 
 
   stationName = 'SoCal Television Breaking News LIVE'

39c4209c141f7a0c3ef29585f3da5d8db99b16a6
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-05-30 19:25:05 -0700

    added initial advertising advertising text binding

--- src/app/advertise/advertise.component.ts
+++ src/app/advertise/advertise.component.ts
@@ -12,4 +12,8 @@ export class AdvertiseComponent implements OnInit {
   ngOnInit() {
   }
 
+  p1 = "Advertise Statistics for the Inland Empire Television Network - SoCalTelevision.com"
+  p2 = "No advertising medium has grown as fast as the Internet. More than 72% of Americans have Internet access. On the average users spend a minimum of 9.8 hours a week online. The projected number of Internet users surged to 320 million in 2003, and is well above this mark today!"
+  p3 = "The SoCal Television audience is comprised of 58% Male and 42% Female ranging in age from 16 to 48. This demographic is dramatically increasing as technology advances and becomes more accessible to the general public."
+  
 }

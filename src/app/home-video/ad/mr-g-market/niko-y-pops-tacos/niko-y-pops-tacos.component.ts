541a88762d2d49b309b765d52067dff3ea56145e
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-07-01 21:14:02 -0700

    updated niko y pops tacos social sharing text

--- src/app/home-video/ad/mr-g-market/niko-y-pops-tacos/niko-y-pops-tacos.component.ts
+++ src/app/home-video/ad/mr-g-market/niko-y-pops-tacos/niko-y-pops-tacos.component.ts
@@ -25,5 +25,15 @@ export class NikoYPopsTacosComponent implements OnInit {
   }
   stationName = "Grand Opening: Taco Tuesday at Niko Y Pop's Tacos"
   description = "Niko Y Pop's Tacos"
-  subdescription = "Located inside Mr. G's Market in Corona, Niko Y Pop's Tacos is open daily at 11am, offering authentic fresh Mexican Cuisine that will leave you coming back for more! Listen to the locals, and Try Niko Y Pop's Tacos Today!!! Visit Niko Y Pop's Tacos is located at 1411 Rimpau Ave #101, Corona CA (Inside Mr. G's Market)"
+  subdescription = "Located inside Mr. G's Market in Corona, Niko Y Pop's Tacos is open daily at 11am, offering authentic fresh Mexican Cuisine that will leave you coming back for more! Listen to the locals, and Try Niko Y Pop's Tacos Today!!! Visit Niko Y Pop's Tacos, located at 1411 Rimpau Ave #101, Corona CA (Inside Mr. G's Market)"
+
+  // Social Sharing binding data
+  title = "GrandOpening: Niko Y Pop's Tacos"
+  header_link = "GrandOpening: Niko Y Pop's Tacos"
+  url = "https://www.socaltelevision.com/+#+/mr-g-market/niko-y-pops-tacos"
+  summary = this.subdescription
+  source_url = this.url
+  subject = this.header_link
+  body = this.subdescription
+
 }

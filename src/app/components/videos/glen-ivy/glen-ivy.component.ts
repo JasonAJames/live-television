3bf8658f6e2eeb6fd3807ba243797cc9a88d6701
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-08-06 21:41:22 -0700

    added Glen Ivy Hot Springs videoEnd function to Glen Ivy Video component

--- src/app/components/videos/glen-ivy/glen-ivy.component.ts
+++ src/app/components/videos/glen-ivy/glen-ivy.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-glen-ivy',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlenIvyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  videoEnd() {
+    // do init at here for current route.
+    console.log('videoEnd function fired.');
+    setTimeout(() => {
+      console.log('fired inside setTimeout function.');
+      this.router.navigate(['home-video-1']);
+      console.log('Should have redirected by now.');
+  }, );  // ,1000 1s
   }
 
 }
